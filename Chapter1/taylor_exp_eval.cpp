f3f66c2a5ee2dabc50b10bbe26c8c7abdeaf07ac
hmageste <henrique.mageste@gmail.com>
2016-04-13 10:54:18 -0300

    Changes in taylor

--- Chapter1/taylor_exp_eval.cpp
+++ Chapter1/taylor_exp_eval.cpp
@@ -2,7 +2,7 @@
 #include<cmath>
 #include<cstdlib>
 
-int factorial( int n )
+long int factorial( int n )
 {
     return (n == 1 || n == 0) ? 1 : factorial(n-1)*n;
 }
@@ -10,7 +10,7 @@ int factorial( int n )
 int main( int argc, char* argv[] )
 {
     int n = std::atoi(argv[2]);
-    double exp_x, x = std::atof(argv[1]);
+    long double exp_x, x = std::atof(argv[1]);
     for ( int i = 0; i < n; i++ )
 	exp_x += pow( x, i )/factorial(i);
 
